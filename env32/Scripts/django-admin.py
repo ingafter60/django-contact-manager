d0b33d61177fe1aab4a893e3c01730f9fbd952ac
ing <inyoman_gurnitha@yahoo.com>
2019-03-04 16:14:29 +0700

    Initial commit

---
+++ env32/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\inyom\desktop\django2-contactmanager\contact-manager\env32\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
